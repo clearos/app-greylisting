d0a163a3ca2b72a44c557ece4ef1babb27b6fe30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:15:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'די גרייליסטינג אַפּ גיט נאָך שוץ קעגן ספּאַם און ווירוסעס דורך פֿילטרירונג אויס ניט-געהאָרכיק E- פּאָסט אַרטיקלען.';
+$lang['greylisting_app_name'] = 'גרייליסטינג';
+$lang['greylisting_delay'] = 'פאַרהאַלטן';
+$lang['greylisting_delay_invalid'] = 'פאַרהאַלטן איז פאַרקריפּלט.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'מאַקסימום ריטענשאַן צייַט איז פאַרקריפּלט.';
+$lang['greylisting_retention_time'] = 'רעטענטיאָן Time';
