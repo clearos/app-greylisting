653bf1357686dcecda552b13af06f9206921e9c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:06:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = '灰名单应用程序过滤掉不符合标准的电子邮件提供针对垃圾邮件和病毒提供额外的保护。';
+$lang['greylisting_app_name'] = '灰名单';
+$lang['greylisting_delay'] = '延迟';
+$lang['greylisting_delay_invalid'] = '延迟无效。';
+$lang['greylisting_maximum_retention_time_invalid'] = '最大的保留时间是无效的。';
+$lang['greylisting_retention_time'] = '保留时间';
