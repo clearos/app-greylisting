d7c12ee61ff269253e861541f4be4420a7dec94d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:13:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'The app e Greylisting sireletsa tsa tlatsetso khahlanong spam-hloko ke filtara melaetsa non-tletlebo e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'ho tšenyo ea nako';
+$lang['greylisting_delay_invalid'] = 'Ho tšenyo ea nako ha o sebetse.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Palo e kahodimodimo tshegetsa le go tlotla nako ha o sebetse.';
+$lang['greylisting_retention_time'] = 'tshegetsa le go tlotla Time';
