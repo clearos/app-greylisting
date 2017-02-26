984a519e1e50472637eed44abb322096095017f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:10:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting അപ്ലിക്കേഷൻ നോൺ-എച്ഐഡിയ്ക്കുചിതമായൊരു ഇ-മെയിൽ സന്ദേശങ്ങൾ ഫിൽറ്റർ സ്പാം വൈറസുകളും നേരെ അധിക പരിരക്ഷ നൽകുന്നു.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'കാലതാമസം';
+$lang['greylisting_delay_invalid'] = 'കാലതാമസം അസാധുവാണ്.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'പരമാവധി നിലനിർത്തൽ സമയം അസാധുവാണ്.';
+$lang['greylisting_retention_time'] = 'നിലനിർത്തൽ സമയം';
