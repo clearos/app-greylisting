adc5341b14335d6f9e695d79feccea1c66a6ab5e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:11:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Il-app Greylisting tipprovdi protezzjoni addizzjonali kontra l-ispam u viruses bil-filtrazzjoni messaġġi mhux konformi e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'dewmien';
+$lang['greylisting_delay_invalid'] = 'Dewmien huwa invalidu.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Ħin massimu ta &#39;żamma huwa invalidu.';
+$lang['greylisting_retention_time'] = 'iż-Żamma Ħin';
