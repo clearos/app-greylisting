3ee382d25f88374863eacfdd0f55419c71c3a7c3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:10:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting app nodrošina papildu aizsardzību pret surogātpastu un vīrusiem, izfiltrējot neatbilstošu e-pasta ziņojumus.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'aizkavēšana';
+$lang['greylisting_delay_invalid'] = 'Kavēšanās ir nederīgs.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maksimālais aiztures laiks ir nederīgs.';
+$lang['greylisting_retention_time'] = 'aiztures laiks';
