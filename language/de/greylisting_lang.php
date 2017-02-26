0a382ae8f36d0a09c93e2c863217f18c56df3d87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:07:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Die Graue Liste App bietet zusätzlichen Schutz vor Spam und Viren durch Ausfiltern nicht konforme E-Mail-Nachrichten.';
+$lang['greylisting_app_name'] = 'Graue Liste';
+$lang['greylisting_delay'] = 'Verzögern';
+$lang['greylisting_delay_invalid'] = 'Delay ist ungültig.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maximale Aufbewahrungsdauer ist ungültig.';
+$lang['greylisting_retention_time'] = 'Retentionszeit';
