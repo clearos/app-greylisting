e7467dc1f98866d4b0f0f15f63ad4966e832eb74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:08:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'The daftar abu-abu app memberikan perlindungan tambahan terhadap spam dan virus dengan menyaring pesan non-compliant e-mail.';
+$lang['greylisting_app_name'] = 'daftar abu-abu';
+$lang['greylisting_delay'] = 'Menunda';
+$lang['greylisting_delay_invalid'] = 'Delay tidak valid.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'waktu retensi maksimum tidak valid.';
+$lang['greylisting_retention_time'] = 'retensi Waktu';
