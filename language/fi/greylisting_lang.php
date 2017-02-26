93080821438d21e8e4a4448c77e48d29e7bdd262
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:07:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Harmaalla sovellus tarjoaa lisäsuojaa roskapostia ja viruksia suodattamalla pois vaatimustenvastainen sähköpostiviestejä.';
+$lang['greylisting_app_name'] = 'Harmaalistauksessa';
+$lang['greylisting_delay'] = 'Viive';
+$lang['greylisting_delay_invalid'] = 'Viive on virheellinen.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Suurin retentioaika on virheellinen.';
+$lang['greylisting_retention_time'] = 'retentioaika';
