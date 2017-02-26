865b7a3a2ab9aeba6f812e35942d8eef8666745e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:04:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'التطبيق القائمة الرمادية يوفر حماية إضافية ضد الفيروسات والبريد المزعج من خلال تصفية رسائل البريد الإلكتروني غير متوافقة.';
+$lang['greylisting_app_name'] = 'القائمة الرمادية';
+$lang['greylisting_delay'] = 'تأخير';
+$lang['greylisting_delay_invalid'] = 'تأخير غير صالح.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'الوقت الاحتفاظ الأقصى غير صالح.';
+$lang['greylisting_retention_time'] = 'الاحتفاظ الوقت';
