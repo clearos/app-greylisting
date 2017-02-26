a84661dfd852ff2b3a4e282cb811e51578d0f04c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:11:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Aplikasi Greylisting menyediakan perlindungan tambahan terhadap spam dan virus dengan menapis mesej e-mel yang tidak patuh.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'kelewatan';
+$lang['greylisting_delay_invalid'] = 'Kelewatan adalah tidak sah.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'masa tahanan maksimum adalah tidak sah.';
+$lang['greylisting_retention_time'] = 'penahanan Masa';
