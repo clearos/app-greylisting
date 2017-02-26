8912958e4edc101700fd3ae11d5cf17c28e4486d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:04:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'App Greylisting ofron mbrojtje shtesë kundër spam dhe viruseve nga filtrim mesazhe jo-përputhje e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'vonesë';
+$lang['greylisting_delay_invalid'] = 'Vonesa është e pavlefshme.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'koha maksimale e mbajtjes është e pavlefshme.';
+$lang['greylisting_retention_time'] = 'Ruajtja Koha';
