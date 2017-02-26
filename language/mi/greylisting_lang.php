96ebcf70fe2958dd4116567c5fb11397afabe40c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:10:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'whakarato te taupānga Greylisting tiaki atu ki pāme me huaketo i tātari i ngā karere kore-ture ī-mēra.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'whakaroa';
+$lang['greylisting_delay_invalid'] = 'He muhu whakaroa.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'He muhu te wā pupuri mōrahi.';
+$lang['greylisting_retention_time'] = 'Wā Pupuru';
