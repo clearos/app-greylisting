a886956697e77f3448ba819543262af8d28df73d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:08:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'A Greylisting alkalmazás további védelmet nyújt a levélszemét és a vírusok kiszűrésével nem megfelelő e-mail üzeneteket.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Késleltetés';
+$lang['greylisting_delay_invalid'] = 'Delay érvénytelen.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'A maximális tartózkodási idő érvénytelen.';
+$lang['greylisting_retention_time'] = 'Késleltetési idő';
