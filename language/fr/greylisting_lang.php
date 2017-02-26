ee5560305db99ba9a951e708075eb200b6a609ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:07:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'L&#39;application Greylisting offre une protection supplémentaire contre le spam et les virus en filtrant les messages non conformes e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Retard';
+$lang['greylisting_delay_invalid'] = 'Le délai est invalide.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'temps de rétention maximale est invalide.';
+$lang['greylisting_retention_time'] = 'Temps de rétention';
