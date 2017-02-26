71c5db28b012a94b3e54cfb30fd15f9668296f4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:08:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'The Greylisting app veitir frekari vernd gegn ruslpósti og vírusum með því að sía út óhlýðin tölvupóstskeyti.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'töf';
+$lang['greylisting_delay_invalid'] = 'Delay er ógilt.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Hámarks rástími er ógilt.';
+$lang['greylisting_retention_time'] = 'retention time';
