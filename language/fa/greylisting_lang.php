21b21c7edb40f9914b48a895adff3a470f39b824
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:12:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'برنامه Greylisting فراهم می کند حفاظت در برابر هرزنامه ها و ویروس از طریق فیلتر کردن پیام های غیر سازگار ایمیل.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'تاخیر انداختن';
+$lang['greylisting_delay_invalid'] = 'تاخیر نامعتبر است.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'حداکثر زمان ماند نامعتبر است.';
+$lang['greylisting_retention_time'] = 'زمان تماس';
