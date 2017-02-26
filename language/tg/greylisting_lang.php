e54e7c4ce6912d17f3e20bc620f5cd7a178b9142
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:14:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Дар барнома Greylisting таъмин ҳифзи иловагӣ зидди спам ва вирусҳо бо полоиши берун паёмҳои ғайри мутобиқ почтаи электронӣ.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'таъхир';
+$lang['greylisting_delay_invalid'] = 'Таъхир нодуруст аст.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Максимум вақти нигоҳдории нодуруст аст.';
+$lang['greylisting_retention_time'] = 'Нигоҳдорӣ Time';
