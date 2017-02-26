8ecdc63d4c4a95348b22117d2360da95cc5a55ff
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:09:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting Қолданба емес үйлесімді электрондық пошта хабарларын сүзу арқылы спам мен вирустардан қосымша қорғауды қамтамасыз етеді.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'кешіктіру';
+$lang['greylisting_delay_invalid'] = 'Delay жарамсыз болып табылады.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Ең ұстау уақыты жарамсыз.';
+$lang['greylisting_retention_time'] = 'Сақтау уақыты';
