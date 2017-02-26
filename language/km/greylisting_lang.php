c2409338a72572da7329d76d997871b95b1b183c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:09:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'កម្មវិធី Greylisting ផ្តល់នូវការការពារបន្ថែមទៀតប្រឆាំងនឹងសារឥតបានការនិងមេរោគដោយការត្រងចេញសារដែលមិនបានគោរពតាមអ៊ីមែល។';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'ការពន្យាពេល';
+$lang['greylisting_delay_invalid'] = 'ការពន្យាពេលមិនត្រឹមត្រូវ។';
+$lang['greylisting_maximum_retention_time_invalid'] = 'ពេលឃាត់អតិបរមាមិនត្រឹមត្រូវ។';
+$lang['greylisting_retention_time'] = 'ការឃាត់ម៉ោង';
