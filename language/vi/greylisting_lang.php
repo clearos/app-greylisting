b36fccf65c9db063e9fea73e709cf810505664f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:15:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Các ứng dụng Greylisting cung cấp bảo vệ bổ sung chống lại thư rác và virus bằng cách lọc ra các thông điệp không tuân thủ e-mail.';
+$lang['greylisting_app_name'] = 'greylisting';
+$lang['greylisting_delay'] = 'Sự chậm trễ';
+$lang['greylisting_delay_invalid'] = 'Sự chậm trễ là không hợp lệ.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'thời gian lưu tối đa là không hợp lệ.';
+$lang['greylisting_retention_time'] = 'Thời gian lưu';
