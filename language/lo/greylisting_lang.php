61ab21964bbd6ebb7529e2a48761438ca2c5c69f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:10:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'ການ app Greylisting ໃຫ້ການປົກປ້ອງຈາກສະແປມແລະໄວຣັສໂດຍການກັ່ນຕອງອອກຂໍ້ຄວາມທີ່ບໍ່ແມ່ນ compliant e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'ຄວາມຊັກຊ້າ';
+$lang['greylisting_delay_invalid'] = 'ຊັກຊ້າບໍ່ຖືກຕ້ອງ.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'ທີ່ໃຊ້ເວລາການຮັກສາໄວ້ສູງສຸດແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['greylisting_retention_time'] = 'ທີ່ໃຊ້ເວລາການຮັກສາໄວ້';
