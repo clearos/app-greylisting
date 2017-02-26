bc45dfd0e02ee0922fe32fcc988f542ff396659b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:11:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting अनुप्रयोग अनुवर्ती-नसलेल्या ई-मेल संदेश फिल्टर स्पॅम आणि व्हायरस विरुद्ध अतिरिक्त संरक्षण प्रदान करते.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'विलंब';
+$lang['greylisting_delay_invalid'] = 'विलंब अवैध आहे.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'जास्तीत जास्त धारणा वेळ अवैध आहे.';
+$lang['greylisting_retention_time'] = 'अवधारण काळ';
