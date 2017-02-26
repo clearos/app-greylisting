3a4f0c427db2df5c64a4bb240f706bc2ae749ad8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:16:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Uhlelo lokusebenza Greylisting isivikelo ngogaxekile namagciwane okufika imiyalezo non-imithetho e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Siphele';
+$lang['greylisting_delay_invalid'] = 'Siphele ayivumelekile.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maximum nokugcinwa kwesikhathi ayivumelekile.';
+$lang['greylisting_retention_time'] = 'Ukugcinwa Isikhathi';
