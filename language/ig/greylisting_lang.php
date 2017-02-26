46f35f61102818654c1267f7b8cfed314a6ce418
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:08:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'The Greylisting ngwa-enye ndị ọzọ nchedo spam na nje site nzacha si na-abụghị Dabara e-mail ozi.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'igbu oge';
+$lang['greylisting_delay_invalid'] = 'Igbu oge bụ ghara ịdị irè.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Oke njigide oge bụ ghara ịdị irè.';
+$lang['greylisting_retention_time'] = 'njigide Time';
