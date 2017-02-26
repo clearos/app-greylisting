c1d456c3ee10a555f64cecde8ad0ad78dc74dc7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:10:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Ny Greylisting fanampiny fampiharana manome fiarovana hanohitra amin&#39;ny spam sy ny virosy ny fanivànana avy tsy compliant e-mail hafatra.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'avy hatrany';
+$lang['greylisting_delay_invalid'] = 'Dia tsy manan-kery avy hatrany.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maximum fihazonana ny fotoana dia tsy manan-kery.';
+$lang['greylisting_retention_time'] = 'fihazonana Time';
