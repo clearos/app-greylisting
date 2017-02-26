73ea817aafdc01929e5b8721a17008bb064cd80f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:13:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'app Greylisting wuxuu bixiyaa difaac dheeraad ah ka gees ah spam iyo fayrusyada by dabaqii baxay fariimaha aan waafaqsaneyn e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Dib';
+$lang['greylisting_delay_invalid'] = 'Dib waa baadil.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'waqti haynta ugu badan waa baadil.';
+$lang['greylisting_retention_time'] = 'haynta Time';
