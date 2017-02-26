7c1666b885f5333180796bc245afed212161254a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:09:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = '에 Greylisting 앱이 호환되지 않는 전자 메일 메시지를 필터링하여 스팸 및 바이러스에 대한 추가 보호 기능을 제공합니다.';
+$lang['greylisting_app_name'] = '그레이 리스팅';
+$lang['greylisting_delay'] = '지연';
+$lang['greylisting_delay_invalid'] = '지연이 잘못되었습니다.';
+$lang['greylisting_maximum_retention_time_invalid'] = '최대 체류 시간이 잘못되었습니다.';
+$lang['greylisting_retention_time'] = '유지 시간';
