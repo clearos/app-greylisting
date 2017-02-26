c38a81c8f294a4b31e127ce093f4587d4583ff60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:09:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting app უზრუნველყოფს დამატებით დაცვას სპამის და ვირუსების მიერ ფილტრაცია არასამთავრობო compliant ელ შეტყობინებები.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Delay';
+$lang['greylisting_delay_invalid'] = 'Delay არასწორია.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'მაქსიმალური შეკავება დრო არასწორია.';
+$lang['greylisting_retention_time'] = 'შეკავების დრო';
