18006673982bf2146f03a11f5d08456c5ddd38e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:07:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'The Greylisting app bayar da ƙarin kariya da spam da ƙwayoyin cuta da tace daga wadanda ba mai yarda e-mail saƙonni.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'bata lokaci';
+$lang['greylisting_delay_invalid'] = 'Bata lokaci ba daidai ba ne.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maximum riƙewa lokaci ba daidai ba ne.';
+$lang['greylisting_retention_time'] = 'riƙewa Time';
