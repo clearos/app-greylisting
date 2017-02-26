f19a9d04d52f532809579560e75cdc68f905d5ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:15:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Greylisting ilova nodavlat mos e-mail xabarlarini filtrlash tomonidan spam va viruslar qarshi qo&#39;shimcha himoya qilish imkonini beradi.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'Delay';
+$lang['greylisting_delay_invalid'] = 'Delay haqiqiy emas.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Maksimal saqlash muddati yaroqsiz hisoblanadi.';
+$lang['greylisting_retention_time'] = 'saqlash Time';
