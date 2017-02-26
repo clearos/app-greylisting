d50d2a2055788aa4a63454f66c645779f086beb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:12:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Aplicația Greylisting oferă o protecție suplimentară împotriva spam-ului și a virușilor, prin filtrarea mesajelor care nu respectă cerințele de e-mail.';
+$lang['greylisting_app_name'] = 'greylisting';
+$lang['greylisting_delay'] = 'Întârziere';
+$lang['greylisting_delay_invalid'] = 'Întârziere este nevalid.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'Timpul maxim de retenție este nevalid.';
+$lang['greylisting_retention_time'] = 'Timp de retenție';
