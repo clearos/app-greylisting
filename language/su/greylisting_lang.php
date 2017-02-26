623908cd11b8b7c2259738fe0d2f2058e6566c0f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 10:13:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/greylisting_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['greylisting_app_description'] = 'Nu Greylisting app nyadiakeun panyalindungan tambahan ngalawan spam jeung virus ku Disaring kaluar talatah non-patuh e-mail.';
+$lang['greylisting_app_name'] = 'Greylisting';
+$lang['greylisting_delay'] = 'nunda';
+$lang['greylisting_delay_invalid'] = 'Reureuh anu sah.';
+$lang['greylisting_maximum_retention_time_invalid'] = 'waktu ingetan nu maksimum téh sah.';
+$lang['greylisting_retention_time'] = 'ingetan Time';
